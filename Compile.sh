abc0c9e05648bd80c39a88668b93f9fd81cc8eee
SkillerRaptor <skillerraptor@protonmail.com>
2020-10-23 00:47:38 +0200

    [Files] Removing Old Files

--- Compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-i686-elf-as boot.s -o boot.o
-i686-elf-g++ -c kernel.cpp -mno-red-zone -o kernel.o -ffreestanding -O2 -Wall -Wextra -fno-exceptions -fno-rtti
-i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
-qemu-system-i386 -kernel myos.bin
