9f13ccf63ac66ead35c163373c14146bea1fe531
SkillerRaptor <skillerraptor@protonmail.com>
2020-10-26 20:55:37 +0100

    [LibC] Adding size_t for 32 & 64 Bit

--- Libraries/LibC/stdint.h
+++ Libraries/LibC/stdint.h
@@ -12,6 +12,14 @@ typedef signed int int32_t;
 typedef signed short int16_t;
 typedef signed char int8_t;
 
+#ifdef defined(HYPER_32)
+/* 32 Bit */
+typedef int32_t size_t;
+#elif defined(HYPER_64)
+/* 64 Bit */
+typedef int64_t size_t;
+#endif
+
 #define INT8_MIN (-128)
 #define INT16_MIN (-32767-1)
 #define INT32_MIN (-2147483647-1)
