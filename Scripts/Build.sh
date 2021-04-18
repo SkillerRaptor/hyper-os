84524c25bf0adb5c484684291012a0c548e74261
SkillerRaptor <skillerraptor@protonmail.com>
2021-04-18 02:38:36 +0200

    [Build] Fixing limine build

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -8,6 +8,7 @@ dd if=/dev/zero bs=1M count=0 seek=64 of=$KERNEL_HDD
 parted -s $KERNEL_HDD mklabel msdos
 parted -s $KERNEL_HDD mkpart primary 1 100%
 $THIRD_PARTY_HOME/echfs/echfs-utils -m -p0 $KERNEL_HDD quick-format 32768
-$THIRD_PARTY_HOME/echfs/echfs-utils -m -p0 $KERNEL_HDD import Kernel.elf build/Kernel.elf
+$THIRD_PARTY_HOME/echfs/echfs-utils -m -p0 $KERNEL_HDD import Kernel.elf Kernel.elf
 $THIRD_PARTY_HOME/echfs/echfs-utils -m -p0 $KERNEL_HDD import limine.cfg limine.cfg
+$THIRD_PARTY_HOME/echfs/echfs-utils -m -p0 $KERNEL_HDD import limine.sys limine.sys
 $THIRD_PARTY_HOME/limine/bin/limine-install $KERNEL_HDD
