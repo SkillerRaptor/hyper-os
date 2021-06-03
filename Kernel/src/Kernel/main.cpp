e3e358e869df078789021c4fbbb1fa72e6aff4e2
SkillerRaptor <skillerraptor@protonmail.com>
2021-06-03 02:28:48 +0200

    [Kernel] Adding fancy header logging

--- Kernel/src/Kernel/main.cpp
+++ Kernel/src/Kernel/main.cpp
@@ -22,6 +22,20 @@ namespace Kernel
 		AK::Serial::initialize();
 		AK::Logger::info("HyperOS is booting...");
 		
+		auto* firmware_tag = reinterpret_cast<stivale2_struct_tag_firmware*>(stivale2_get_tag(bootloader_data, STIVALE2_STRUCT_TAG_FIRMWARE_ID));
+		
+		AK::Logger::info(" _  _                     ___  ___    ");
+		AK::Logger::info("| || |_  _ _ __  ___ _ _ / _ \\/ __|  ");
+		AK::Logger::info("| __ | || | '_ \\/ -_) '_| (_) \\__ \\");
+		AK::Logger::info("|_||_|\\_, | .__/\\___|_|  \\___/|___/");
+		AK::Logger::info("      |__/|_|                         ");
+		AK::Logger::info("");
+		AK::Logger::info("Firmware: %s", (firmware_tag->flags & STIVALE2_FIRMWARE_BIOS) ? "BIOS" : "UEFI");
+		AK::Logger::info("Bootloader: %s %s", bootloader_data->bootloader_brand, bootloader_data->bootloader_version);
+		AK::Logger::info("");
+		AK::Logger::info("------------------------------------");
+		AK::Logger::info("");
+		
 		GDT::initialize();
 		PIC::remap(0x20, 0x28);
 		IDT::initialize();
