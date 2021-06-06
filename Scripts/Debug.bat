9a428f4e72e98761497594ffb2f96ad5219c593b
SkillerRaptor <skillerraptor@protonmail.com>
2021-06-06 13:27:46 +0200

    [General] Removing host specific things

--- Scripts/Debug.bat
+++ Scripts/Debug.bat
@@ -1,3 +1,6 @@
 @echo off
 
+SET BASEDIR=%~dp0
+cd %BASEDIR%..
+
 qemu-system-x86_64 -vga std -drive format=raw,media=disk,index=0,file=./Build/Kernel.hdd -d int,cpu_reset -serial stdio -no-reboot -no-shutdown -m 256 -accel tcg -smp 4
--- Scripts/Run.sh
+++ Scripts/Run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+BASEDIR=$(dirname "$0")
+cd "$BASEDIR"/.. || exit
+
 qemu-system-x86_64 -vga std -drive format=raw,media=disk,index=0,file=./Build/Kernel.hdd -serial stdio -m 256 -soundhw pcspk -enable-kvm -smp 4
