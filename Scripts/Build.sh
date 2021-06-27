609570241e9438caf880dead02cad180fe3d943c
SkillerRaptor <skillerraptor@protonmail.com>
2021-06-27 14:55:11 +0200

    [Scripts] Removing Arch x86_32

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -1,34 +1,21 @@
 #!/bin/bash
 
 build_step() {
-    NAME=$1
-    shift
-    "$@" 2>&1 | sed $'s|^|\x1b[34m['"${NAME}"']\x1b[39m |'
+  NAME=$1
+  shift
+  "$@" 2>&1 | sed $'s|^|\x1b[34m['"${NAME}"']\x1b[39m |'
 }
 
 build_error() {
-    build_step HyperOS echo "Building failed!"
-    exit
+  build_step HyperOS echo "Building failed!"
+  exit
 }
 
 build_step HyperOS echo "Building HyperOS..."
 
-arch=x86_64
-
-if [ "$1" ] 
-  then
-    if [ $1 != "x86_64" ] && [ $1 != "x86_32" ] 
-      then
-        build_step HyperOS echo "Unknown architecture $1"
-        build_error
-      else
-        arch="$1"
-    fi
-fi
-
 if [[ "$OSTYPE" == "darwin"* ]]; then
   realpath() {
-      [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+    [[ $1 == /* ]] && echo "$1" || echo "$PWD/${1#./}"
   }
 
   cores=$(sysctl -n hw.physicalcpu)
@@ -38,11 +25,11 @@ fi
 
 true_path="$(dirname "$(realpath "$0")")"
 root_path=$true_path/..
-pushd $root_path > /dev/null
-build_step Bash mkdir -p Build/$arch || build_error
-pushd Build/$arch > /dev/null
+pushd $root_path >/dev/null
+build_step Bash mkdir -p Build || build_error
+pushd Build >/dev/null
 build_step CMake cmake --build . --parallel $cores || build_error
-popd > /dev/null
-popd > /dev/null
+popd >/dev/null
+popd >/dev/null
 
 build_step HyperOS echo "Building done!"
--- Scripts/Configure.sh
+++ Scripts/Configure.sh
@@ -1,38 +1,25 @@
 #!/bin/bash
 
 configure_step() {
-    NAME=$1
-    shift
-    "$@" 2>&1 | sed $'s|^|\x1b[34m['"${NAME}"']\x1b[39m |'
+  NAME=$1
+  shift
+  "$@" 2>&1 | sed $'s|^|\x1b[34m['"${NAME}"']\x1b[39m |'
 }
 
 configure_error() {
-    configure_step HyperOS echo "Configuring failed!"
-    exit
+  configure_step HyperOS echo "Configuring failed!"
+  exit
 }
 
 configure_step HyperOS echo "Configuring HyperOS..."
 
-arch=x86_64
-
-if [ "$1" ] 
-  then
-    if [ $1 != "x86_64" ] && [ $1 != "x86_32" ] 
-      then
-        configure_step HyperOS echo "Unknown architecture $1"
-        configure_error
-      else
-        arch="$1"
-    fi
-fi
-
 true_path="$(dirname "$(realpath "$0")")"
 root_path=$true_path/..
-pushd $root_path > /dev/null
-configure_step Bash mkdir -p Build/$arch || configure_error
-pushd Build/$arch > /dev/null
-configure_step CMake cmake ./../.. -G "Ninja" -DARCH=$arch || build_error
-popd > /dev/null
-popd > /dev/null
+pushd $root_path >/dev/null
+configure_step Bash mkdir -p Build || configure_error
+pushd Build >/dev/null
+configure_step CMake cmake ./../.. -G "Ninja" || build_error
+popd >/dev/null
+popd >/dev/null
 
 configure_step HyperOS echo "Configuring done!"
