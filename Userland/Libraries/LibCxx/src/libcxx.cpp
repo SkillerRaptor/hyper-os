5cc122ba5b5308d6026c8974a901c77b480608d2
SkillerRaptor <skillerraptor@protonmail.com>
2021-06-30 19:51:22 +0200

    [Userland] Adding basic LibCxx

---
+++ Userland/Libraries/LibCxx/src/libcxx.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020-2021, SkillerRaptor <skillerraptor@protonmail.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+/* temporary source file */
+#include <array>
+#include <cassert>
+#include <cstddef>
+#include <iterator>
+#include <type_traits>
