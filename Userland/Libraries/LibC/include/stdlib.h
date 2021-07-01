acea1d81db9dc40741e61b83541328d3d1dde272
SkillerRaptor <skillerraptor@protonmail.com>
2021-07-01 14:47:54 +0200

    [LibCpp] Adding "new" header

--- Userland/Libraries/LibC/include/stdlib.h
+++ Userland/Libraries/LibC/include/stdlib.h
@@ -7,11 +7,17 @@
 #ifndef HYPEROS_LIBC_STDLIB_H_
 #define HYPEROS_LIBC_STDLIB_H_
 
+#include <stddef.h>
 #include <sys/cdefs.h>
 
 __BEGIN_DECLS
 
 __attribute__((__noreturn__)) void abort(void);
+
+void* malloc(size_t size);
+void* calloc(size_t num, size_t size);
+void* realloc(void* ptr, size_t size);
+void free(void* ptr);
 
 __END_DECLS
 
--- Userland/Libraries/LibC/src/stdlib.c
+++ Userland/Libraries/LibC/src/stdlib.c
@@ -5,14 +5,37 @@
  */
 
 #include <stdlib.h>
+#include <string.h>
 
 void abort(void)
 {
 	// TODO: Implement abort
-	
+
 	while (1)
 	{
 		asm volatile("cli");
 		asm volatile("hlt");
 	}
+}
+
+void* malloc(size_t size)
+{
+	// TODO: Implement malloc
+}
+
+void* calloc(size_t num, size_t size)
+{
+	void* ptr = malloc(num * size);
+	memset(ptr, 0x00, num * size);
+	return ptr;
+}
+
+void* realloc(void* ptr, size_t size)
+{
+	// TODO: Implement realloc
+}
+
+void free(void* ptr)
+{
+	// TODO: Implement free
 }
