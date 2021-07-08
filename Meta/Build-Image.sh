bd146d2809f6a6df7223227bf04da0a5df94d8f3
SkillerRaptor <skillerraptor@protonmail.com>
2021-07-08 17:11:28 +0200

    [Meta] Updating scripst with AHCI & NVMe & USB drive
    Creating NVMe & USB image for QEMU
    QEMU will now detect a AHCI, NVMe and USB drive

--- Meta/Build-Image.sh
+++ Meta/Build-Image.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 KERNEL_HDD=$HYPEROS_BINARY_DIR/Root/boot/kernel.hdd
+NVME_IMG=$HYPEROS_BINARY_DIR/Root/boot/nvme.img
+USB_IMG=$HYPEROS_BINARY_DIR/Root/boot/usb.img
 ECHFS_COMMAND=$HYPEROS_SOURCE_DIR/ThirdParty/echfs/echfs-utils
 LIMINE_INSTALL_COMMAND=$HYPEROS_SOURCE_DIR/ThirdParty/limine/limine-install-linux-x86_64
 
-rm -f "$KERNEL_HDD"
+rm -f "$KERNEL_HDD" "$NVME_IMG" "$USB_IMG"
 dd if=/dev/zero bs=1M count=0 seek=64 of="$KERNEL_HDD"
+dd if=/dev/zero bs=1M count=0 seek=128 of="$NVME_IMG"
+dd if=/dev/zero bs=1M count=0 seek=128 of="$USB_IMG"
 parted -s "$KERNEL_HDD" mklabel msdos
 parted -s "$KERNEL_HDD" mkpart primary 1 100%
 "$ECHFS_COMMAND" -m -p0 "$KERNEL_HDD" quick-format 32768
--- Meta/Run.sh
+++ Meta/Run.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-qemu-system-x86_64 -vga std -drive format=raw,media=disk,index=0,file="$HYPEROS_BINARY_DIR/Root/boot/kernel.hdd" -serial stdio -m 256 -soundhw pcspk -enable-kvm -smp 4
+qemu-system-x86_64 \
+  -drive file=nvme.img,if=none,id=NVME1,format=raw \
+  -device nvme,drive=NVME1,serial=nvme \
+  -drive file="$HYPEROS_BINARY_DIR/Root/boot/kernel.hdd",if=none,id=disk,format=raw \
+  -device ahci,id=ahci \
+  -device ide-hd,drive=disk,bus=ahci.0 \
+  -drive file=usb.img,if=none,id=usbstick,format=raw \
+  -usb \
+  -vga std \
+  -serial stdio \
+  -m 256 \
+  -enable-kvm \
+  -smp 4
